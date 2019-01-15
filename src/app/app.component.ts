4a765b1e477072da704d8acf12184b4f351c3d4a
Michał Łyczko <lyczos@gmail.com>
2019-01-15 13:16:11 +0100

    add comment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular';
+  // This content should come from remote source:
   testHtml = `
   <h2>H2 and interpolaton: 2+2={{2+2}}</h2>
   <hello-dynamic [testInput]="'yeahString'"></hello-dynamic>
